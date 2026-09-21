3c912244b3b6cc9505141eda25914e0031404797
maprunty <maprunty@student.42heilbronn.de>
2026-09-21 19:51:24 +0200

    feat(models): Added __str__ method to FnModel

--- rank_03/call_me_maybe/src/models.py
+++ rank_03/call_me_maybe/src/models.py
@@ -7,18 +7,20 @@
 #    By: maprunty <maprunty@student.42heilbronn.d  +#+  +:+       +#+         #
 #                                                +#+#+#+#+#+   +#+            #
 #    Created: 2026/09/13 09:39:52 by maprunty         #+#    #+#              #
-#    Updated: 2026/09/13 12:20:12 by maprunty        ###   ########.fr        #
+#    Updated: 2026/09/21 19:16:36 by maprunty        ###   ########.fr        #
 #                                                                             #
 # *************************************************************************** #
 
 
 from typing import Any
 
-from pydantic import BaseModel, model_validator
+from pydantic import BaseModel, Field, model_validator
 
 _TYPES = {
     "string": str,
     "number": float,
+    "boolean": bool,
+    "integer": int,
 }
 
 
@@ -44,7 +46,11 @@ class FnModel(BaseModel):
         return values
 
     def __str__(self) -> str:
-        return self.name
+        return (
+            f"- {self.name}"
+            + f"({', '.join(f'{k}: {v.__name__}' for k, v in self.parameters.items())}): "
+            + self.description
+        )
 
     def __getattr__(self, name):
         return self.name
@@ -67,5 +73,22 @@ class OutModel(BaseModel):
     """Pydantic model for an output."""
 
     prompt: str
-    name: str
-    parameters: dict[str, Any]
+    name: str = Field(default="")
+    parameters: dict[str, Any] = Field(default_factory=dict)
+
+    def __str__(self) -> str:
+        msg_ai = ""
+        #        r_str = (
+        #            f"Prompt: {self.prompt if self.prompt else msg_ai}",
+        #            f"Name: {self.name if self.name else msg_ai}\n",
+        #            f"Parameters: {self.parameters if self.parameters else msg_ai}\n",
+        #        )
+        r_str = (
+            f"{'Prompt: ' + self.prompt if self.prompt else msg_ai}\n"
+            + f"{'Name: ' + self.name if self.name else msg_ai}\n"
+            + f"{'Parameters: ' + str(self.parameters) if self.parameters else msg_ai}\n"
+        )
+        return r_str
+
+    def __iter__(self):
+        return [self.prompt, self.name, iter(self.parameters.items())]
